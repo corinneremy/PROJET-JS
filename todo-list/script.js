a53264592e1a842bb99299bad222b8859901b767
komiJESUS <127386761+komiJESUS@users.noreply.github.com>
2023-03-29 16:23:27 +0200

    update

--- todo-list/script.js
+++ todo-list/script.js
@@ -15,6 +15,13 @@ addToDoButton.onclick = function(){
     //value this paragraph with input content  
     paragraph.innerText = inputField.value ;
 
+    //paragraphe style
+    paragraph.classList.add('paragraph_style');
+
     // insert paragraph into toDoContainer element
     toDoContainer.appendChild(paragraph);
+
+    //empty input when paragraph is added
+    inputField.value = "";
+
 }
