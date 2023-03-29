d4dea3d8d7a53713b87eeb6d933d40b896c78a7d
corinneremy <corinne7086@hotmail.fr>
2023-03-29 15:46:04 +0200

    toDoList

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let citation[
+["On ne se baigne jamais deux fois dans le même fleuve "]
+["Je sais que je ne sais rien "]
+["L'homme est un animal politique" ]
+["La mort n'est rien pour nous " ]
+["Je pense, donc je suis "]
+]
