cccb64023004825724dbd7660696dcdacd443412
komiJESUS <127386761+komiJESUS@users.noreply.github.com>
2023-03-30 09:35:48 +0200

    final

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,27 +1,34 @@
+const addToDoButton = document.querySelector("#addToDo");
+const toDoContainer = document.querySelector("#toDoContainer");
+const inputField = document.querySelector("#inputField");
 
-const addToDoButton = document.querySelector('#addToDo');
-const toDoContainer = document.querySelector('#toDoContainer');
-const inputField = document.querySelector('#inputField');
+addToDoButton.onclick = function () {
+  let paragraph = null;
+  console.log(inputField.value);
+  // check if input is not empty
+  if (inputField.value != "") {
+    // if input is not empty, create a paragraph
+    paragraph = document.createElement("p");
+  }
+  //value this paragraph with input content
+  paragraph.innerText = inputField.value;
 
+  //paragraphe style
+  paragraph.classList.add("paragraph_style");
 
-addToDoButton.onclick = function(){
-    let paragraph = null;
-    console.log(inputField.value);
-    // check if input is not empty
-    if(inputField.value != ""){
-        // if input is not empty, create a paragraph
-        paragraph = document.createElement('p')
-    }
-    //value this paragraph with input content  
-    paragraph.innerText = inputField.value ;
+  // insert paragraph into toDoContainer element
+  toDoContainer.appendChild(paragraph);
 
-    //paragraphe style
-    paragraph.classList.add('paragraph_style');
+  //empty input when paragraph is added
+  inputField.value = "";
 
-    // insert paragraph into toDoContainer element
-    toDoContainer.appendChild(paragraph);
+  //line-through paragraph when click
+  paragraph.addEventListener("click", function () {
+    paragraph.classList.toggle("paragraph_click");
+  });
 
-    //empty input when paragraph is added
-    inputField.value = "";
-
-}
+  //delete task when double click on it
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
+};
